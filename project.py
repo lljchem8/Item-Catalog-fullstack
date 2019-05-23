af7c486765d47a5634d504519d9c2bd0295d0efa
Liji Le <lljchem8@gmail.com>
2019-05-23 19:28:56 -0400

    added post, delete, edit method for catalog

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -11,11 +11,42 @@ app = Flask(__name__)
 def showCatalog():
     return "show all kinds of catalog"
 
+# show all catalog
+
 
 @app.route('/catalog/<string:name>')
 @app.route('/catalog/<string:name>/items')
 def showCatalogName(name):
     return "catalog {}".format(name)
+
+# add a new catalog
+
+
+@app.route('/catalog/new', methods=['GET', 'POST'])
+def newCatalog():
+    if (request.method == 'POST'):
+        return "a new catalog added"
+    else:
+        return "its not a valid post request"
+
+
+# delete a catalog
+@app.route('/catalog/<string:name>/delete', methods=['GET', 'POST'])
+def deleteCatalog(name):
+    if (request.method == 'POST'):
+        return "a catalog is deleted"
+    else:
+        return "not a valid post request"
+
+# edit a catalog
+
+
+@app.route('/catalog/<string:name>/edit', methods=['GET', 'POST'])
+def editCatalog(name):
+    if (request.method == 'POST'):
+        return "the catalog has been edited"
+    else:
+        return "not a valid request"
 
 
 @app.route('/catalog/<string:catName>/<string:itemName>')
